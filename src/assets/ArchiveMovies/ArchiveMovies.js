f1cd6b1ecd6f594a13ffe54c6f02ad2993fa353a
Drako9159 <chochito9159@gmail.com>
2022-05-20 22:19:28 -0500

    add css

---
+++ src/assets/ArchiveMovies/ArchiveMovies.js
@@ -0,0 +1,306 @@
+const movies = [
+    {
+        "name": "Al Filo Del Invierno",
+        "img": "alFiloDelInvierno.png",
+        "path": "alFiloDelInvierno.mp4"
+    },{
+        "name": "Antes De Ti",
+        "img": "antesDeTi.png",
+        "path": "antesDeTi.mp4"
+    },{
+        "name": "Better Days",
+        "img": "betterDays.png",
+        "path": "betterDays.mp4"
+    },{
+        "name": "Bob Esponja Heres",
+        "img": "bobEsponjaHeroesAlRescate.png",
+        "path": "bobEsponjaHeroesAlRescate.mp4",
+    },{
+        "name": "Caminando Entre Tumbas",
+        "img": "caminandoEntreTumbas.png",
+        "path": "caminandoEntreTumbas.mp4"
+    },{
+        "name": "Chronicle",
+        "img": "chronicle.png",
+        "path": "chronicle.mp4"
+    },{
+        "name": "Coda",
+        "img": "coda.png",
+        "path": "coda.mp4"
+    },{
+        "name": "Cosmoball",
+        "img": "cosmoball.png",
+        "path": "cosmoball.mp4"
+    },{
+        "name": "Drunk",
+        "img": "druk.png",
+        "path": "druk.mp4"
+    },{
+        "name": "Dune",
+        "img": "dune.png",
+        "path": "dune.mp4"
+    },{
+        "name": "El Chico Que Salvo La Navidad",
+        "img": "elChicoQueSalvoLaNavidad.png",
+        "path": "elChicoQueSalvoLaNavidad.mp4"
+    },{
+        "name": "El Dragón De Los Deseos",
+        "img": "elDragonDeLosDeseos.png",
+        "path": "elDragonDeLosDeseos.mp4"
+    },{
+        "name": "El Hombre Del Agua",
+        "img": "elHombreDelAgua.png",
+        "path": "elHombreDelAgua.mp4"
+    },{
+        "name": "elIncidente",
+        "img": "elIncidente.png",
+        "path": "elIncidente.mp4"
+    },{
+        "name": "El Metodo Williams",
+        "img": "elMetodoWilliams.png",
+        "path": "elMetodoWilliams.mp4"
+    },{
+        "name": "El Oficial Y El Espía",
+        "img": "elOficialYElEspia.png",
+        "path": "elOficialYElEspia.mp4"
+    },{
+        "name": "El Olivo Que Seremos",
+        "img": "elOlvidoQueSeremos.png",
+        "path": "elOlvidoQueSeremos.mp4"
+    },{
+        "name": "El Padrino",
+        "img": "elPadrino.png",
+        "path": "elPadrino.mp4"
+    },{
+        "name": "El Protector",
+        "img": "elProtector.png",
+        "path": "elProtector.mp4"
+    },{
+        "name": "EL Robo Del Siglo",
+        "img": "elRoboDelSiglo.png",
+        "path": "elRoboDelSiglo.mp4"
+    },{
+        "name": "El Telefono",
+        "img": "elTelefono.png",
+        "path": "elTelefono.mp4"
+    },{
+        "name": "Encanto",
+        "img": "encanto.png",
+        "path": "encanto.mp4"
+    },{
+        "name": "Escuadron Suicida",
+        "img": "escuadronSuicida.png",
+        "path": "escuadronSuicida.mp4"
+    },{
+        "name": "Eternals",
+        "img": "eternals.png",
+        "path": "eternals.mp4"
+    },{
+        "name": "Fallout",
+        "img": "fallout.png",
+        "path": "fallout.mp4"
+    },{
+        "name": "Gran Torino",
+        "img": "granTorino.png",
+        "path": "granTorino.mp4"
+    },{
+        "name": "Historia De Un Matrimonio",
+        "img": "historiaDeUnMatrimonio.png",
+        "path": "historiaDeUnMatrimonio.mp4"
+    },{
+        "name": "Ice Road",
+        "img": "iceRoad.png",
+        "path": "iceRoad.mp4"
+    },{
+        "name": "Intensamente",
+        "img": "intensamente.png",
+        "path": "intensamente.mp4"
+    },{
+        "name": "Aventuras de Bruck",
+        "img": "iceAgeLasAventurasDeBruck.png",
+        "path": "iceAgeLasAventurasDeBruck.mp4"
+    },{
+        "name": "Jorge El Curioso",
+        "img": "jorgeElCurioso.png",
+        "path": "jorgeElCurioso.mp4"
+    },{
+        "name": "Koma",
+        "img": "koma.png",
+        "path": "koma.mp4"
+    },{
+        "name": "La Abuela",
+        "img": "laAbuela.png",
+        "path": "laAbuela.mp4"
+    },{
+        "name": "La Masacre De Texas",
+        "img": "laMasacreDeTexas2022.png",
+        "path": "laMasacreDeTexas2022.mp4"
+    },{
+        "name": "La Mujer Del Espía",
+        "img": "laMujerDelEspia.png",
+        "path": "laMujerDelEspia.mp4"
+    },{
+        "name": "La Patrulla Canina",
+        "img": "laPatrullaCanina.png",
+        "path": "laPatrullaCanina.mp4"
+    },{
+        "name": "Los Minions",
+        "img": "losMinions.png",
+        "path": "losMinions.mp4"
+    },{
+        "name": "Los Olchis",
+        "img": "losOlchis.png",
+        "path": "losOlchis.mp4"
+    },{
+        "name": "Minari",
+        "img": "minari.png",
+        "path": "minari.mp4"
+    },{
+        "name": "Nadie",
+        "img": "nadie.png",
+        "path": "nadie.mp4"
+    },{
+        "name": "Mickey Mouse Christmas",
+        "img": "mickeyAndMinieChristmas.png",
+        "path": "mickeyAndMinieChristmas.mp4"
+    },{
+        "name": "No Mires Arriba",
+        "img": "noMiresArriba.png",
+        "path": "noMiresArriba.mp4"
+    },{
+        "name": "No Respires 2",
+        "img": "noRespires2.png",
+        "path": "noRespires2.mkv"
+    },{
+        "name": "Noticias Del Gran Mundo",
+        "img": "noticiasDelGranMundo.png",
+        "path": "noticiasDelGranMundo.mp4"
+    },{
+        "name": "No Todos Pudimos Madurar",
+        "img": "noTodosPudimosMadurar.png",
+        "path": "noTodosPudimosMadurar.mp4"
+    },{
+        "name": "Polvo",
+        "img": "polvo.png",
+        "path": "polvo.mp4"
+    },{
+        "name": "Prisioneros",
+        "img": "prisioneros.png",
+        "path": "prisioneros.mp4"
+    },{
+        "name": "Puñales Por La Espalda",
+        "img": "punalesPorLaEspalda.png",
+        "path": "punalesPorLaEspalda.mp4"
+    },{
+        "name": "RayaEl Último Dragón",
+        "img": "rayaElUltimoDragon.png",
+        "path": "rayaElUltimoDragon.mp4"
+    },{
+        "name": "Red",
+        "img": "red.png",
+        "path": "red.mp4"
+    },{
+        "name": "Ron Da Error",
+        "img": "ronDaError.png",
+        "path": "ronDaError.mp4"
+    },{
+        "name": "Shang",
+        "img": "shang.png",
+        "path": "shang.mp4"
+    },{
+        "name": "Sing 2",
+        "img": "sing2.png",
+        "path": "sing2.mp4"
+    },{
+        "name": "Siniestro",
+        "img": "siniestro.png",
+        "path": "siniestro.mp4"
+    },{
+        "name": "Siniestro 2",
+        "img": "siniestro2.png",
+        "path": "siniestro2.mp4"
+    },{
+        "name": "Spiderman Sin Camino",
+        "img": "spidermanSinCaminoACasa.png",
+        "path": "spidermanSinCaminoACasa.mkv"
+    },{
+        "name": "Tigre Blanco",
+        "img": "tigreBlanco.png",
+        "path": "tigreBlanco.mp4"
+    },{
+        "name": "Titanic",
+        "img": "titanic.png",
+        "path": "titanic.mp4"
+    },{
+        "name": "Toy Story 1",
+        "img": "toyStory1.png",
+        "path": "toyStory1.mp4"
+    },{
+        "name": "Toy Story 2",
+        "img": "toyStory2.png",
+        "path": "toyStory2.mp4"
+    },{
+        "name": "Toy Story 3",
+        "img": "toyStory3.png",
+        "path": "toyStory3.mp4"
+    },{
+        "name": "Toy Story 4",
+        "img": "toyStory4.png",
+        "path": "toyStory4.mp4"
+    },{
+        "name": "Hotel Transilvania 4",
+        "img": "transilvania4.png",
+        "path": "transilvania4.mp4"
+    },{
+        "name": "Upgrade",
+        "img": "upgrade.png",
+        "path": "upgrade.mp4"
+    },{
+        "name": "Venganza",
+        "img": "venganza.png",
+        "path": "venganza.mp4"
+    },{
+        "name": "VHS94",
+        "img": "vhs94.png",
+        "path": "vhs94.mp4"
+    }
+]
+
+//ChargeImagesnumbe
+for(let imgCharge = 0; imgCharge <= 65; imgCharge = imgCharge + 1 ){
+    let ostec = `img${imgCharge}`;
+    let doc = document.getElementById(ostec);
+    let checkMov = movies[imgCharge].img;
+    let pathImg = `./img/${checkMov}`;
+    doc.src = pathImg;
+    console.log(pathImg);
+}
+function play(number){
+
+    document.getElementById("closeMov0").className = "closeMovOn"
+    document.getElementById("media0").className = "mediaOn"
+    document.getElementById("video0").className = "videoOn"
+    document.getElementById("nameMov0").className = "nameMovOn"
+    document.getElementById("dropImg0").className = "dropImgOff"
+    document.getElementById("dropImg1").className = "dropImgOff"
+    document.getElementById("dropImg2").className = "dropImgOff"
+    document.getElementById("dropImg3").className = "dropImgOff"
+    
+    let needVideo = movies[number].path
+    let again = `./video/${needVideo}`
+    document.getElementById("video0").src = again
+    let needName = movies[number].name
+    document.getElementById("nameMov0").innerHTML = needName
+}
+function mediaOff(){
+    document.getElementById("closeMov0").className = "closeMov0"
+    document.getElementById("media0").className = "media0"
+    document.getElementById("video0").className = "video0"
+    document.getElementById("video0").pause()
+    document.getElementById("nameMov0").className = "nameMov0"
+    document.getElementById("dropImg0").className = "dropImg0"
+    document.getElementById("dropImg1").className = "dropImg0"
+    document.getElementById("dropImg2").className = "dropImg0"
+    document.getElementById("dropImg3").className = "dropImg0"
+}
+
--- src/assets/NewContent/NewContent.js
+++ src/assets/NewContent/NewContent.js
@@ -75,15 +75,29 @@ for(let imgCharge = 0; imgCharge <= 15; imgCharge = imgCharge + 1 ){
     console.log(pathImg);
 }
 function play(number){
+    document.getElementById("closeMov0").className = "closeMovOn"
     document.getElementById("media0").className = "mediaOn"
     document.getElementById("video0").className = "videoOn"
+    document.getElementById("nameMov0").className = "nameMovOn"
+    document.getElementById("dropImg0").className = "dropImgOff"
+    document.getElementById("dropImg1").className = "dropImgOff"
+    document.getElementById("dropImg2").className = "dropImgOff"
+    document.getElementById("dropImg3").className = "dropImgOff"
     let needVideo = movies[number].path
     let again = `../src/assets/NewContent/video/${needVideo}`
     document.getElementById("video0").src = again
+    let needName = movies[number].name
+    document.getElementById("nameMov0").innerHTML = needName
 }
 function mediaOff(){
+    document.getElementById("closeMov0").className = "closeMov0"
     document.getElementById("media0").className = "media0"
     document.getElementById("video0").className = "video0"
     document.getElementById("video0").pause()
+    document.getElementById("nameMov0").className = "nameMov0"
+    document.getElementById("dropImg0").className = "dropImg0"
+    document.getElementById("dropImg1").className = "dropImg0"
+    document.getElementById("dropImg2").className = "dropImg0"
+    document.getElementById("dropImg3").className = "dropImg0"
 }
 
